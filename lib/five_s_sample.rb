8c14feeee586398386b60d4c429efb57b1b06936
k2works <kakimomokuri@gmail.com>
2014-09-05 12:04:57 +0900

    done:整頓

--- lib/five_s_sample.rb
+++ lib/five_s_sample.rb
@@ -1,19 +1,32 @@
 # encoding: utf-8
 
 class FiveS_sample
-  def method01
+
+#  def method01
+#    "10"
+#  end
+
+#  def method02(arg)
+#    "10"
+#  end
+
+#  def method03
+#    "1"
+#  end
+
+#  def method04(arg)
+#    "10"
+#  end
+
+  def return_ten
     "10"
   end
 
-  def method02(arg)
+  def return_ten_with_arg(arg)
     "10"
   end
 
-  def method03
+  def return_one
     "1"
-  end
-
-  def method04(arg)
-    "10"
   end
 end
