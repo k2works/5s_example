1899d6b8a15205c58444c5a6925f17265e7a1b23
k2works <kakimomokuri@gmail.com>
2014-09-05 12:12:36 +0900

    done:清掃

--- lib/five_s_sample.rb
+++ lib/five_s_sample.rb
@@ -2,22 +2,6 @@
 
 class FiveS_sample
 
-#  def method01
-#    "10"
-#  end
-
-#  def method02(arg)
-#    "10"
-#  end
-
-#  def method03
-#    "1"
-#  end
-
-#  def method04(arg)
-#    "10"
-#  end
-
   def return_ten
     "10"
   end
